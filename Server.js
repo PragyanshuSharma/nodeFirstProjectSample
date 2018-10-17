d61e6bb9b8c9363955dbd98ca276b8febe9aed3c
rishabh <sharad15897@gmail.com>
2018-10-17 23:55:34 +0530

    backend complete

--- Server.js
+++ Server.js
@@ -1,4 +1,6 @@
 const express=require("express");
+const bodyparser = require('body-parser');
+
 
 var db = require('knex')({
     client: 'sqlite3',
@@ -8,19 +10,44 @@ var db = require('knex')({
   });
 
 const app=express();
+app.use(bodyparser());
 
 app.get('/',(req,res)=>{
  res.send('Root Directory');
 })
 
 
-app.get('/select',(req,res)=>{
+app.post('/first',(req,res)=>{
+  var obj = {
+    status:'Wrong Password',
+    userid:''
+  };
+
+  let flag = '';
+ const {user,password} = req.body;
+    console.log(" user , password" , user,password);
+
+    
+
   db('persons')
-  .then(data=>console.log(data[0].UserID , data[0].Password ))
-  .catch(err=>console.log(err))
-  
-  res.send('Sub root Directory');
-  
+  .where('UserID','=',user)
+  .where('Password','=',password)
+  .then(data=>{
+    console.log("data is ",data);
+    console.log("length is ",data.length); 
+    if(data.length===1){
+        flag=  'correct';
+        res.json({ status:'Correct' });
+      }
+     else
+     flag='Wrong'; 
+     res.json({ status :'Wrong'});  
+    })
+   .catch(err=>{
+    console.log('error ');
+  })
+
+    
  })
 
 
--- main.js
+++ main.js
@@ -4,4 +4,8 @@ function validate()
     var id=document.getElementById("1").value;
     var pswd=document.getElementById("2").value;
     alert("Hello user"+id+ pswd);
+
+        
+ 
+
 }
