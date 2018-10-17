b21c195e68928faff7a7b717dec8e73226b8ead1
rishabh <sharad15897@gmail.com>
2018-10-17 19:08:13 +0530

    database added

--- Server.js
+++ Server.js
@@ -1,8 +1,21 @@
 const express=require("express");
 
+var knex = require('knex')({
+    client: 'sqlite3',
+    connection: {
+      filename:'/home/rishabh/Documents/Node.js/Database/mydatabase.sqlite';
+    
+    }
+  });
+
 const app=express();
+
+app.get('/',(req,res)=>{
+ res.send('Root Directory');
+})
 app.listen(3000,()=>{
-    console.log("Server has started")
+    console.log("Server has started");
+
 }
 );
 
--- main.js
+++ main.js
@@ -3,5 +3,5 @@ function validate()
     alert("Hello");
     var id=document.getElementById("1").value;
     var pswd=document.getElementById("2").value;
-    alert("Aagye Bc"+id+ pswd);
+    alert("Hello user"+id+ pswd);
 }
